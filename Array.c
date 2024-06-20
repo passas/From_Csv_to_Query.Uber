7977bb3c4486ce82fdec9c9efad87bb7130749d4
Sérgio Costa <48985218+passas@users.noreply.github.com>
2024-06-20 19:19:33 +0100

    Array void**

---
+++ Array.c
@@ -0,0 +1,82 @@
+#include "Array.h"
+
+#include <stdlib.h>
+
+
+// Further encapsulation
+
+
+Array *new_array (int capacity)
+{
+    Array *array;
+    
+    // Array alloc
+    array = NULL;
+    array = (struct array *) malloc (sizeof (struct array));
+    
+    if ( array )
+    {
+        // Array init
+        array->i = 0;
+        array->capacity = capacity;
+
+        // Array storage
+        array->data = (void *) malloc (sizeof (void *) * capacity);
+        // to-do: error handling
+    }
+    // to-do: error handling
+
+    return array;
+}
+
+void add_array (Array *array, void *data)
+{
+    // Array is full
+    if (array->i == array->capacity)
+    {
+        void **tmp;
+
+        // Realloc
+        tmp = NULL;
+        tmp = (void *) reallocarray ( array->data, array->capacity*2, sizeof (void *) );
+
+        if ( tmp )
+        {
+            array->capacity <<= 1; // capacity * 2
+            array->data = tmp;
+        }
+        //to-do: error handling
+    }
+
+    if (array->i < array->capacity) // Safe-guard
+    {
+        // Add element
+        array->data [ array->i ] = data;
+        // Update current index
+        array->i++;
+    }
+}
+
+void *get_array (Array a, int pos)
+{
+    if (pos < a.i)
+        return ( a.data[pos] );
+    else
+        return NULL;
+}
+
+int length_array (Array a)
+{
+    return ( a.i );
+}
+
+void free_array (Array *a)
+{
+    if ( a )
+    {
+        // Array storage
+        free (a->data);
+        // Array structure
+        free ( a );
+    }
+}
