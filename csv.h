8b48c440c915e6cc6ace85fd0330f0767e39b595
Sérgio Costa <48985218+passas@users.noreply.github.com>
2024-06-20 21:29:19 +0100

    struct buffer_csv

---
+++ csv.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <regex.h>
+
+
+#ifndef _CSV_H_
+#define _CSV_H_
+
+struct buffer_csv
+{
+    char from_file[75];
+
+    // Read
+    FILE *file_pointer;
+    size_t line_length;
+    char *line_content;
+    
+    // Handle
+    char delim[2];
+    int total_columns;
+    regex_t *token_regex; //REG_EXTENDED | REG_ICASE
+    
+    // Suit
+    char **line_token;
+};
+
+
+struct buffer_csv *new_buffer_csv (char filename[], int columns, char *regex[], char delim[2]);
+
+void init_buffer_csv (struct buffer_csv *);
+
+int tokenize_csv_line (struct buffer_csv *);
+
+void end_buffer_csv (struct buffer_csv *);
+
+void free_buffer_csv (struct buffer_csv *);
+
+
+#endif
